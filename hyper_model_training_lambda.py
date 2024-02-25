6ac9e984b0199d5570283618e356b9a0f45b7fe5
gjliesen <46409098+gjliesen@users.noreply.github.com>
2024-02-25 02:28:44 -0700

    minor changes to directory variables

--- hyper_model_training_lambda.py
+++ hyper_model_training_lambda.py
@@ -5,15 +5,20 @@ from sim_network_engine import SimNetworkEngine
 from sim_test_engine import SimTestEngine
 
 # Create the directories needed to store model data
-SimNetworkEngine.create_directories()
+tuner_dir = "/home/ubuntu/aircraft-nn-training-data/tuner"
+model_dir = "/home/ubuntu/aircraft-nn-training-data/models"
+checkpoint_dir = "/home/ubuntu/aircraft-nn-training-data/model_checkpoints"
+training_dir = "/home/ubuntu/aircraft-nn-training-data"
+
+SimNetworkEngine.create_directories(tuner_dir, model_dir, checkpoint_dir)
 
 date_str = datetime.now().strftime("%m%d%Y_%H%M%S")
 project_name = f"{date_str}_aircraft_sim"
 batch_size = 2048
 
 # Load the training and validation datasets
-train_dataset = SimDataEngine.load_dataset("Train").cache().batch(batch_size)
-val_dataset = SimDataEngine.load_dataset("Val").cache().batch(batch_size)
+train_dataset = SimDataEngine.load_dataset("Train", dir=training_dir).cache().batch(batch_size)
+val_dataset = SimDataEngine.load_dataset("Val", dir=training_dir).cache().batch(batch_size)
 
 # Specifying non-default hyperparameters
 loss_fn = SimNetworkEngine.root_mean_squared_error
@@ -22,7 +27,7 @@ activation_functions = ["relu", "tanh", "sigmoid"]
 hypermodel_fn = SimNetworkEngine.get_hypermodel_fn(loss_fn, activation_functions)
 # tune the hypermodel
 tuner = SimNetworkEngine.build_tuner(
-    hypermodel_fn, "hyperband", project_name, max_epochs=10
+    hypermodel_fn, "hyperband", project_name, max_epochs=10, directory=tuner_dir
 )
 tuner = SimNetworkEngine.tune_model(tuner, train_dataset, val_dataset, es_patience=4)
 
@@ -40,6 +45,8 @@ hypermodel, history = SimNetworkEngine.train_tuned_model(
     tuner=tuner,
     model_name="aircraft_sim_1",
     tuner_trial=1,
+    checkpoint_dir=checkpoint_dir,
+    model_dir=model_dir
 )
 SimNetworkEngine.plot_network_history(history)
 
@@ -50,6 +57,8 @@ hypermodel, history = SimNetworkEngine.train_tuned_model(
     tuner=tuner,
     model_name="aircraft_sim_2",
     tuner_trial=2,
+    checkpoint_dir=checkpoint_dir,
+    model_dir=model_dir
 )
 SimNetworkEngine.plot_network_history(history)
 
@@ -60,6 +69,8 @@ hypermodel, history = SimNetworkEngine.train_tuned_model(
     tuner=tuner,
     model_name="aircraft_sim_3",
     tuner_trial=3,
+    checkpoint_dir=checkpoint_dir,
+    model_dir=model_dir
 )
 SimNetworkEngine.plot_network_history(history)
 
@@ -70,6 +81,8 @@ hypermodel, history = SimNetworkEngine.train_tuned_model(
     tuner=tuner,
     model_name="aircraft_sim_4",
     tuner_trial=4,
+    checkpoint_dir=checkpoint_dir,
+    model_dir=model_dir
 )
 SimNetworkEngine.plot_network_history(history)
 
