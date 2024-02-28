4e45192b2f3924aace95a1b0564f532d7e2cee68
gjliesen <46409098+gjliesen@users.noreply.github.com>
2024-02-28 00:17:49 -0700

    set up sim data engine to work with config files

--- sim_data_engine.py
+++ sim_data_engine.py
@@ -1,130 +1,105 @@
+import pstats
 import time
+import json
 import os
 import tensorflow as tf
 import numpy as np
+from numpy.typing import ArrayLike
 from simulation.aircraft_sim_no_forces import AircraftSimNoForces
 from datetime import datetime
-
-
-def timeit(f):
-    def timed(*args, **kw):
-        ts = time.time()
-        result = f(*args, **kw)
-        te = time.time()
-        print(f"func:{f.__name__} took: {te-ts} sec")
-        return result
-
-    return timed
 
 
 class SimDataEngine:
     def __init__(
         self,
-        config,
-        constraints,
-        rate,
-        time,
-        iterations,
-        rnd_method="random",
-        title="sim_output",
-        shuffle=False,
+        config_name: str,
+        constraints: dict,
+        frequency: float,
+        time: int,
+        iterations: int,
+        test_cases: int,
+        length: int,
+        rnd_method: str,
+        shuffle: bool,
+        INPUT_DIM: int,
+        OUTPUT_DIM: int,
+        NETWORK_TYPE: str,
     ):
-        self.DEFAULTS = config["DEFAULTS"]
-        self.DIR = config["DIR"]
-        self.date_str = datetime.now().strftime("%m%d%Y_%H%M%S")
+        self.config_name = config_name
         self.constraints = constraints
-        self.title = title
-        self.rate = rate
+        self.frequency = frequency
         self.time = time
         self.iterations = iterations
+        self.test_cases = test_cases
+        self.length = length
         self.rnd_method = rnd_method
-        self.t_vec = np.arange(0, self.time + self.rate, self.rate)[5:-10]
         self.shuffle = shuffle
+        self.t_vec = np.arange(0, time + frequency, frequency)
 
-    @timeit
-    def create_dataset(self, purpose="train"):
+        self.INPUT_DIM = INPUT_DIM
+        self.OUTPUT_DIM = OUTPUT_DIM
+        self.NETWORK_TYPE = NETWORK_TYPE
+
+        self.date_str = datetime.now().strftime("%m%d%Y_%H%M%S")
+
+        self.data_path = os.path.join(os.getcwd(), "data")
+        self.initialize_directories()
+
+        self.cur_input = None
+        self.cur_output = None
+
+    def initialize_directories(self):
+        os.makedirs(self.data_path, exist_ok=True)
+        folder_name = f"{self.date_str}_{self.config_name}_{self.NETWORK_TYPE}_{self.INPUT_DIM}_to_{self.OUTPUT_DIM}"
+        self.data_path = os.path.join(self.data_path, folder_name)
+        os.makedirs(self.data_path, exist_ok=True)
+
+    def generate_dataset(self):
+        self.write_tfrecords("train")
+        for case in range(self.test_cases):
+            self.write_tfrecords(f"test_{case}")
+
+    def write_tfrecords(self, name: str):
         """Primary function, used to call the aircraft simulation and write the output to a tfrecords file, it will either be a test or training dataset, this determines the folder to write the data to
 
         Args:
-            purpose (str, optional): _description_. Defaults to "train".
+            purpose (str, optional): _description_. NETWORK to "train".
         """
-        print("starting to write records...")
-        if purpose == "Train":
-            dir = "training_data"
+        print(f"starting write {name} records...")
+        record_path = os.path.join(self.data_path, f"{name}.tfrecords")
+        with tf.io.TFRecordWriter(record_path) as tfrecord:
+            for itr in self.iterations:
+                valid = self.run_sim(itr)
+                if valid:
+                    self.write_example(tfrecord)
+                if (itr + 1) % 500 == 0:
+                    print(f"iteration: {itr+1} of {self.iterations}")
+
+    def write_example(self, tfrecord: object):
+        if self.NETWORK_TYPE == "DNN":
+            input_states = self.cur_input
+            output_states = self.cur_output
+        elif self.NETWORK_TYPE == "RNN":
+            input_states = [self.cur_input]
+            output_states = [self.cur_output]
         else:
-            dir = "test_data"
+            raise ValueError(
+                "Invalid configuration. Supported configurations are 'DNN' and 'RNN'."
+            )
 
-        with tf.io.TFRecordWriter(
-            f"{dir}/{self.date_str}_{self.title}_{purpose}.tfrecord"
-        ) as tfrecord:
-            for iter in self.iterations:
-                self.run_sim(iter)
-                if np.isnan(self.tmp_output).any():
-                    print(self.tmp_input)
-                    continue
-                else:
-                    pass
-                if (iter + 1) % 500 == 0:
-                    print(f"iteration: {iter+1} of {self.iterations}")
-
-    def write_example(tfrecord, input_state, output_state):
-        for i in range(len(input_state)):
+        for input_state, output_state in zip(input_states, output_states):
             data = {
                 "input_state": tf.train.Feature(
-                    float_list=tf.train.FloatList(value=input_state[i])
+                    float_list=tf.train.FloatList(value=input_state)
                 ),
                 "output_state": tf.train.Feature(
-                    float_list=tf.train.FloatList(value=output_state[i])
+                    float_list=tf.train.FloatList(value=output_state)
                 ),
             }
             example = tf.train.Example(features=tf.train.Features(feature=data))
-        tfrecord.write(example.SerializeToString())
+            tfrecord.write(example.SerializeToString())
 
-    def create_training_set(
-        self,
-        constraints,
-        rate,
-        time,
-        iters,
-        rnd_method="random",
-        title="sim_output",
-        shuffle=True,
-    ):
-
-        inputs = []
-        outputs = []
-        for iter in range(self.iterations):
-            self.run_sim(iter)
-            if np.isnan(self.tmp_output).any():
-                print(self.tmp_input)
-                continue
-            else:
-                inputs.append(self.tmp_input)
-                outputs.append(self.tmp_output)
-            if (iter + 1) % 500 == 0:
-                print(f"iteration: {iter+1} of {self.iterations}")
-        # Store the numpy versions of the training data in a list
-        input_states = np.vstack(tuple(inputs))
-        output_states = np.vstack(tuple(outputs))
-
-        if self.shuffle:
-            training_split = 0.8
-            split_idx = int(np.floor(len(input_states) * training_split))
-
-            # Write train data
-            self._write_records(
-                input_states[0:split_idx], output_states[0:split_idx], "Train"
-            )
-
-            # Write validation data
-            self._write_records(
-                input_states[split_idx::], output_states[split_idx::], "Val"
-            )
-        else:
-            # Write train data
-            self._write_records(input_states, output_states, "Test")
-
-    def randomize(self, input_tup):
+    def randomize(self, input_tup: tuple):
         rng = np.random.default_rng()
         if self.rnd_method == "random":
             return rng.uniform(input_tup[0], input_tup[1])
@@ -133,7 +108,7 @@ class SimDataEngine:
         else:
             print("unsupported method")
 
-    def run_sim(self, iter):
+    def run_sim(self, itr: int):
         # Initialize the initial conditons using numpy.random.uniform
         ic_lla = np.array(
             [self.randomize(self.constraints[key]) for key in self.constraints]
@@ -142,20 +117,24 @@ class SimDataEngine:
         sim = AircraftSimNoForces(ic_lla)
         # Run the simulation
         sim.run_simulation(self.time, 0, 0, 0, 0, np.array([0, 0, 0]))
-        if (iter + 1) % 10000 == 0:
+        if (itr + 1) % 10000 == 0:
             sim.all_states_graph("verify")
         # Extract data from simulation
         self._extract_sim_data(sim)
+        if np.isnan(self.cur_output).any():
+            print(self.cur_input)
+            return False
+        else:
+            return True
 
-    def _extract_sim_data(self, sim):
+    def _extract_sim_data(self, sim: object):
         # Create lists of runs to vertical stack later
-        self.tmp_input = self.concatInputs(sim.lla_states_init).squeeze()[5:-10]
-        self.tmp_output = sim.get_all_states()[5:-10]
+        self.cur_input = self.concatInputs(sim.lla_states_init).squeeze()
+        self.cur_output = sim.get_all_states()
         if self.shuffle:
             self.shuffle_data()
 
-    # @timeit
-    def concatInputs(self, inits):
+    def concatInputs(self, inits: ArrayLike):
         # Add concatInputs function from old file to stack input instances
         col_concat = np.reshape(inits, (len(inits), 1))
         arr = np.array(
@@ -165,13 +144,13 @@ class SimDataEngine:
 
     def shuffle_data(self):
         # Define indices array
-        indices = np.arange(self.tmp_input.shape[0])
+        indices = np.arange(self.cur_input.shape[0])
         np.random.shuffle(indices)
-        self.tmp_input = self.tmp_input[indices]
-        self.tmp_output = self.tmp_output[indices]
+        self.cur_input = self.cur_input[indices]
+        self.cur_output = self.cur_output[indices]
 
     @staticmethod
-    def map_fn(serialized_example, input_len=14, output_len=13):
+    def map_fn(serialized_example: object, input_len: int = 14, output_len: int = 13):
         feature = {
             "input_state": tf.io.FixedLenFeature([input_len], tf.float32),
             "output_state": tf.io.FixedLenFeature([output_len], tf.float32),
@@ -180,7 +159,7 @@ class SimDataEngine:
         return example["input_state"], example["output_state"]
 
     @staticmethod
-    def normalize_data(example, max_values, invert=False):
+    def normalize_data(example: object, max_values: list, invert=False):
         dim = len(max_values)
         max_values_tensor = tf.constant(max_values, shape=(1, 1, dim), dtype=tf.float32)
         if invert:
@@ -191,13 +170,13 @@ class SimDataEngine:
         return normalized_example
 
     @staticmethod
-    def parse_datetime(filename):
+    def parse_datetime(filename: str):
         date_time_str = filename.split("_")[:2]
         date_time_str = "_".join(date_time_str)
         return datetime.strptime(date_time_str, "%m%d%Y_%H%M%S")
 
     @staticmethod
-    def get_most_recent_dataset(folder_path, dataset_type):
+    def get_most_recent_dataset(folder_path: str, dataset_type: str):
         if dataset_type not in ["Train", "Val"]:
             raise ValueError("dataset_type must be 'Train' or 'Val'")
 
@@ -221,21 +200,21 @@ class SimDataEngine:
 
     @staticmethod
     def load_dataset(
-        purpose,
-        dir="training_data",
-        fname=None,
+        input_norm_factors: list,
+        output_norm_factors: list,
+        dataset_type: str,
+        dir: str = "training_data",
+        fname: str = "",
     ):
-        input_norm_factors["DEFAULTS"]
-        datagen = SimDataEngine()
-        if fname is None:
-            fname = SimDataEngine.get_most_recent_dataset(dir, purpose)
+        if fname == "":
+            fname = SimDataEngine.get_most_recent_dataset(dir, dataset_type)
 
         dataset = tf.data.TFRecordDataset(fname)
-        dataset = dataset.map(datagen.map_fn)
+        dataset = dataset.map(SimDataEngine.map_fn)
         dataset = dataset.map(
             lambda input_state, output_state: (
-                datagen.normalize_data(input_state, input_norm_factors),
-                datagen.normalize_data(output_state, output_norm_factors),
+                SimDataEngine.normalize_data(input_state, input_norm_factors),
+                SimDataEngine.normalize_data(output_state, output_norm_factors),
             )
         )
         return dataset
