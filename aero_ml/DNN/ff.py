7fb85beeb3768ef6cd3e10e258aadf719b8314cb
gjliesen <46409098+gjliesen@users.noreply.github.com>
2024-03-11 00:53:55 -0700

    extended configurator and added default config method

--- aero_ml/DNN/ff.py
+++ aero_ml/DNN/ff.py
@@ -186,6 +186,8 @@ class TestEngine(BaseTestEngine):
 
 
 class Configurator(BaseConfigurator):
+    config: dict
+
     def __init__(self, config_name: str, config_dir: str = "configs"):
         """Configurator for the FF network, sets up default parameter config for network
         but the user can create new ones by calling the methods
@@ -196,98 +198,9 @@ class Configurator(BaseConfigurator):
         """
         super().__init__(config_dir, config_name)
 
-    def general_network(
-        self,
-        input_dim: int = 14,
-        output_dim: int = 13,
-        optimizer: str = "adam",
-        metrics: list[str] = ["mse"],
-        loss_fn_str: str = "mse",
-    ):
-        network = dict(
-            input_dim=input_dim,
-            output_dim=output_dim,
-            optimizer=optimizer,
-            metrics=metrics,
-            loss_fn_str=loss_fn_str,
-        )
 
-        self.set_network(network)
-
-    def general_data(
-        self,
-        maximums_euler: dict = Defaults.MAXIMUMS_EULER,
-        maximums_quat: dict = Defaults.MAXIMUMS_QUAT,
-    ):
-        features_euler = list(maximums_euler.keys())
-        input_dict = {"time": 10}
-        input_dict.update(maximums_quat)
-
-        [*input_features], [*input_norm_factors] = zip(*input_dict.items())
-
-        # Configuring DNN outputs
-        output_dict = maximums_quat
-        [*output_features], [*output_norm_factors] = zip(*output_dict.items())
-
-        data = dict(
-            input_features=input_features,
-            input_features_euler=["Time"] + features_euler,
-            input_norm_factors=input_norm_factors,
-            output_features=output_features,
-            output_features_euler=features_euler,
-            output_norm_factors=output_norm_factors,
-        )
-
-        self.set_data(data)
-
-    def generation_data(
-        self,
-        run_time: int = 10,
-        frequency: float = 0.01,
-        iterations: int = 5000,
-        test_cases: int = 30,
-        constraints: dict = Defaults.constraints,
-        rnd_method: str = "random",
-        shuffle: bool = False,
-    ):
-        data = dict(
-            run_time=run_time,
-            frequency=frequency,
-            iterations=iterations,
-            length=(run_time / frequency) * iterations,
-            test_cases=test_cases,
-            constraints=constraints,
-            rnd_method=rnd_method,
-            shuffle=shuffle,
-        )
-
-        self.set_data(data)
-
-    def tuning_data(
-        self,
-        width_range: list[int] = [20, 50, 2],
-        depth_range: list[int] = [6, 12, 2],
-        activation_fns: list[str] = Defaults.activation_fns,
-        reg_range: list[typing.Union[float, str]] = Defaults.reg_range,
-        kernel_inits: list[str] = Defaults.kernel_inits,
-        bias_inits: list[str] = Defaults.bias_inits,
-        learning_rates: list[float] = Defaults.learning_rates,
-    ):
-        tuner = dict(
-            activation_fns=activation_fns,
-            width_range=width_range,
-            depth_range=depth_range,
-            reg_range=reg_range,
-            kernel_inits=kernel_inits,
-            bias_inits=bias_inits,
-            learning_rates=learning_rates,
-        )
-
-        self.set_tuner(tuner)
-
-
-def generate_default_config():
-    config = Configurator("ff_default")
+def generate_config(config_name: str, config_dir: str):
+    config = Configurator(config_name, config_dir)
     config.general_network()
     config.general_data()
     config.generation_data()
@@ -295,5 +208,8 @@ def generate_default_config():
     config.write()
 
 
-if not os.path.isfile("configs/ff_default.json"):
-    generate_default_config()
+config_name = "ff_default"
+dirname = os.path.join(os.path.dirname(os.path.realpath(__file__)), "configs")
+default_config_path = os.path.join(dirname, f"{config_name}.json")
+if not os.path.isfile(default_config_path):
+    generate_config(config_name, dirname)
